0d9bc857cd4e12b70ed042008ac92ec105cb0a28
Elisha <elishakibet67@gmail.com>
2023-12-11 09:37:45 +0300

    Update.py

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self):
+        self._value = ""
+    
+    # Getters and setters of value string
+    def get_value(self):
+        return self._value
+    
+    def set_value(self, value):
+        if not isinstance(value, str):
+            print("Not of type string, insert string!")
+        else:
+            self._value = value
+
+    value = property(get_value, set_value)
+
+    # other instance methods
+    def is_sentence(self):
+        return self._value.endswith(".")
+    
+    def is_question(self):
+        return self._value.endswith("?")
+    
+    def is_exclamation(self):
+        return self._value.endswith("!")
+    
+    def count_sentences(self):
+        return len([s for s in self._value.split('.') if s.strip() != ''])
+
+word = MyString()
+word.value = "This is a string! It has three sentences. Right?"
+print(word.count_sentences())
